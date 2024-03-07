472cbb57cf02a3d030a49068a9cf64dae4e6a4d7
Ellis <ellispinsky5@gmail.com>
2024-03-06 22:44:30 -0500

    goodreads theme

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      goodreads: {
+        white: '#ffffff', 
+        lightcream: '#e9e5cd',
+        brown: '#75420e',
+        darkbrown: '#553b08',
+        gray: '#aaaaaa',
+      },
+    },},
   },
   plugins: [],
 }
