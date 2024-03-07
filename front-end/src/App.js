0d4094c4ddd50095d9f3f09ba409ec888fb6d7ee
Ellis <ellispinsky5@gmail.com>
2024-03-06 23:09:47 -0500

    refac: modularized routes and added conditional rendering of navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import './index.css';
 
 import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./pages/navbar";
 import SignUpPage from "./pages/Form/SignUpPage";
@@ -14,11 +14,13 @@ import ReadingProgress from "./pages/ReadingProgress";
 import ReadingWishlist from "./pages/ReadingWishlist";
 import CurrentlyReading from "./pages/CurrentlyReading";
 import BookSearch from "./pages/BookSearch";
+import FriendShelf from "./components/FriendShelf";
 
 import ProfilePage from './pages/Form/profile'; // Import the ProfilePage component
 
 
 function App() {
+  
   const [loggedIn, setLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const onUpdateProfile = (updatedInfo) => {
@@ -31,19 +33,34 @@ function App() {
     // update state with search results here once we have backend
   };
 
+  function NavbarWithLocation() {
+    const location = useLocation();
+    if (location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup') {
+      return null;
+    }
+    return <Navbar />;
+  }
+  function AppRoutes() {
+    // All original routes here
+    return (
+      <Routes>
+        <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
+        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/search" element={<BookSearch onSearch={handleBookSearch} />} />
+        <Route path="/mainHome" element={<MainHome />} />
+        <Route path="/edit-profile" element={<EditProfile onUpdateProfile={onUpdateProfile} />} />
+        <Route path="/profile" element={<ProfilePage />} /> {/* Add the route for the ProfilePage */}
+        <Route path="/friend-shelf" element={<FriendShelf />} />
+      </Routes>
+    );
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/search" element={<BookSearch onSearch={handleBookSearch} />} />
-          <Route path="/mainHome" element={<MainHome />} />
-          <Route path="/edit-profile" element={<EditProfile onUpdateProfile={onUpdateProfile} />} />
-          <Route path="/profile" element={<ProfilePage />} /> {/* Add the route for the ProfilePage */}
-        </Routes>
-        {loggedIn && <Navbar />} {/* render Navbar based on loggedIn state */}
+        <AppRoutes />
+        <NavbarWithLocation />
       </BrowserRouter>
     </div>
   );
