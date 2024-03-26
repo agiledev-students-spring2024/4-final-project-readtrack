963c1f9523a60d6c858f8662f76ad27dd28ffcd4
jeffreysaeteros <js11692@nyu.edu>
2024-03-26 00:22:48 -0400

    added useState's for users and profiles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import './index.css';
-
 import { useState } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/home";
@@ -19,20 +18,15 @@ import Friends from "./pages/Friends"
 import Layout from "./components/Layout";
 import Book from "./pages/Book";
 import FriendProfile from "./pages/friendsProfile";
-
-import ProfilePage from './pages/Form/profile'; // Import the ProfilePage component
-
+import ProfilePage from './pages/Form/profile';
 
 function App() {
-
-
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
   const [email, setEmail] = useState("");
+
   const onUpdateProfile = (updatedInfo) => {
     //update user profile here
   };
-
-  
 
   function NavbarWithLocation() {
     const location = useLocation();
@@ -45,13 +39,13 @@ function App() {
   function AppRoutes() {
     return (
       <Routes>
-        <Route path="/" element={<Layout><Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} /></Layout>} />
-        <Route path="/login" element={<Layout><Login setLoggedIn={setLoggedIn} setEmail={setEmail} /></Layout>} />
-        <Route path="/signup" element={<Layout><SignUpPage /></Layout>} />
-        <Route path="/search" element={<Layout><BookSearchPage  /></Layout>} />
+        <Route path="/" element={<Layout><Home email={email} loggedIn={loggedInUser !== null} setLoggedIn={setLoggedInUser} /></Layout>} />
+        <Route path="/login" element={<Layout><Login setLoggedInUser={setLoggedInUser} setEmail={setEmail} /></Layout>} />
+        <Route path="/signup" element={<Layout><SignUpPage setLoggedInUser={setLoggedInUser} /></Layout>} />
+        <Route path="/search" element={<Layout><BookSearchPage /></Layout>} />
         <Route path="/mainHome" element={<Layout> <MainHome /> </Layout>} />
-        <Route path="/edit-profile" element={<Layout> <EditProfile onUpdateProfile={onUpdateProfile} /> </Layout>} />
-        <Route path="/profile" element={<Layout> <ProfilePage /> </Layout>} />
+        <Route path="/edit-profile" element={<Layout> <EditProfile loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} /> </Layout>} />
+        <Route path="/profile" element={<Layout> <ProfilePage loggedInUser={loggedInUser} /> </Layout>} />
         <Route path="/friend-shelf" element={<Layout> <FriendShelf /> </Layout>} />
         <Route path="/friends" element={<Layout> <Friends /> </Layout>} />
         <Route path="/title-by-author" element={<Layout> <Book /> </Layout>} />
