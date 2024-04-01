6077ec873d0ae769b6cdd8f176ec5a4ce3dbf2c1
cnnrdel <cmd9609@nyu.edu>
2024-04-01 19:47:42 -0400

    added standard width to friends on friendshelf

--- front-end/src/components/FriendShelf.js
+++ front-end/src/components/FriendShelf.js
@@ -41,15 +41,19 @@ const FriendShelf = ({ friendsList = friendsReading }) => {
       {friendsList.map((friend) => (
         <div
           key={friend.id}
-          className="min-w-max flex-shrink-0 flex flex-col items-center space-y-2 p-4 border border-gray-200 rounded-lg bg-gray-50 hover:shadow-lg transition duration-200 ease-in-out"
+          className="w-40 flex flex-col items-center space-y-2 p-4 border border-gray-200 rounded-lg bg-gray-50 hover:shadow-lg transition duration-200 ease-in-out"
         >
           <img
             src={friend.friendImage}
             alt={`Avatar of ${friend.friendName}`}
             className="w-24 h-24 object-cover rounded-full border-4 border-white shadow"
           />
-          <h3 className="text-md font-semibold text-gray-900">{friend.friendName}</h3>
-          <p className="text-sm font-medium text-gray-800">{friend.bookTitle}</p>
+          <h3 className="text-md font-semibold text-gray-900">
+            {friend.friendName}
+          </h3>
+          <p className="text-sm font-medium text-gray-800">
+            {friend.bookTitle}
+          </p>
           <p className="text-xs text-gray-600">{friend.bookAuthor}</p>
         </div>
       ))}
@@ -68,19 +72,5 @@ FriendShelf.propTypes = {
     })
   ),
 };
-
-// const FriendShelfContainer = () => {
-//   const [friendsList, setFriendsList] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch data from JSON file
-//     fetch("../data/friends.json")
-//       .then((response) => response.json())
-//       .then((data) => setFriendsList(data))
-//       .catch((error) => console.error("Error fetching data:", error));
-//   }, []);
-
-//   return <FriendShelf friendsList={friendsList} />;
-// };
 
 export default FriendShelf;
