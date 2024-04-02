720807b93890300149e4e7f8a366255e18396148
hah8236 <hah8236@nyu.edu>
2024-04-02 05:08:13 -0400

    very basic testing framework

--- back-end/tests/test.js
+++ back-end/tests/test.js
@@ -1,1 +1,20 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Book Routes', () => {
+    it('should get all books', (done) => {
+        chai.request(app)
+            .get('/books')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+});
+
+// make sure you run `npm install chai@4 chai-http@4 --save-dev` and then run `npm test`
