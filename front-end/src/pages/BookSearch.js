c54bbd3f573825b167ee608b8e18936c67396852
hah8236 <hah8236@nyu.edu>
2024-04-11 14:00:04 -0400

    working on search, not done

--- front-end/src/pages/BookSearch.js
+++ front-end/src/pages/BookSearch.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import SearchComponent from "../components/SearchComponent"; // Adjust the import path as necessary
 import BookShelf from "../components/bookshelf";
 
+// Dummy data lives: in books in back-end app.js
+
 // TODO: Render books on screen
 // TODO: Clicking on book, go to that book's page
 // TODO: Finish search stuff
@@ -12,7 +14,7 @@ import BookShelf from "../components/bookshelf";
 const BookSearchPage = ({ loggedInUser, setLoggedInUser, onSearch }) => { 
 
   const [profile, setProfile] = useState(null);
-  const [topReads, setTopReadss] = useState([]);
+  const [topReads, setTopReads] = useState([]);
   const [books, setBooks] = useState([]);
   const [searchPerformed, setSearchPerformed] = useState(false);
 
@@ -34,16 +36,38 @@ const BookSearchPage = ({ loggedInUser, setLoggedInUser, onSearch }) => {
     const url = `http://localhost:3001/users/${loggedInUser.id}/books/ThisWeek'sTop10Reads`;
     try {
       const topReadsData = fetch(url).then((res) => res.json());
-      setTopReadss(topReadsData)
+      setTopReads(topReadsData)
     } catch (error) {
       console.error("Error fetching book data:", error);
     }
   };
 
-  
+  useEffect(() => {
+    fetchTopReads();
+  }, [loggedInUser]);
+
   
   const handleSearch = (searchTerm) => {
 
+    // filters all books
+    fetch(`http://localhost:3001/books`)
+      .then(response => response.json())
+      .then (data => {
+        const searchResults = data.filter((books) => 
+          books.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setBooks(searchResults);
+        setSearchPerformed(true);
+      })
+      .catch((error) => {
+        console.error("Error searching books:", error);
+      })
+
+    // filter books
+    // put the filtered books into a bookshelf that is able to be displayed
+
+    /*
+    Ellis' work:
 
     // Call the API to search for books 
     const searchUrl =`http://localhost:3001/books/${searchTerm}`;
@@ -54,8 +78,7 @@ const BookSearchPage = ({ loggedInUser, setLoggedInUser, onSearch }) => {
 
       });
       console.log(books)
-      
-    
+      */
   };
   
 
@@ -66,6 +89,7 @@ const BookSearchPage = ({ loggedInUser, setLoggedInUser, onSearch }) => {
       
       <SearchComponent onSearch={handleSearch} />
       {searchPerformed ? (
+        // ok so use the search handling thing to get the books that have that in the title
         <BookShelf title="Search Results" books={books} />
       ) : (
         // before search displays top books
