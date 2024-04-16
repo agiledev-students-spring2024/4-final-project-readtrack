bbd498b49f8475e0898691ae5155acbc58c9fe3c
jeffreysaeteros <js11692@nyu.edu>
2024-04-16 02:07:03 -0400

    changed get /users/:id

--- back-end/src/api/routes/userRoutes.js
+++ back-end/src/api/routes/userRoutes.js
@@ -5,24 +5,24 @@ const userBooksController = require('../controllers/userBooksController')
 const { authenticateToken } = require('../middleware/authMiddleware')
 // Basic user routes
 router.post('/register', userController.register)
-router.post('/login', userController.login)
+router.post('/login', userController.login);
 router.get('/logout', userController.logout)
-router.get('/users', authenticateToken , userController.getAllUsers)
-router.get('/user/:id', authenticateToken , userController.getUser)
-router.put('/user/:id', authenticateToken , userController.updateUser)
-router.delete('/user/:id', authenticateToken , userController.deleteUser)
- 
+router.get('/users', authenticateToken, userController.getAllUsers)
+router.get('/users/:id', authenticateToken, userController.getUser)
+router.put('/user/:id', authenticateToken, userController.updateUser)
+router.delete('/user/:id', authenticateToken, userController.deleteUser)
+
 // not a user route but GENERAL book search
-router.get('/books/search' , authenticateToken,  userBooksController.searchBooks )
+router.get('/books/search', authenticateToken, userBooksController.searchBooks)
 //Nested routes for users books
-router.get('/users/:id/books',authenticateToken, userBooksController.getAllUserBooks)
-router.get('/users/:id/books/currentReads',authenticateToken, userBooksController.getCurrentUserBooks)
+router.get('/users/:id/books', authenticateToken, userBooksController.getAllUserBooks)
+router.get('/users/:id/books/currentReads', authenticateToken, userBooksController.getCurrentUserBooks)
 // want to read
-router.get('/users/:id/books/wantToRead',authenticateToken, userBooksController.getWantToRead)
+router.get('/users/:id/books/wantToRead', authenticateToken, userBooksController.getWantToRead)
 // favorites
-router.get('/users/:id/books/favorites', authenticateToken,userBooksController.getFavorites)
+router.get('/users/:id/books/favorites', authenticateToken, userBooksController.getFavorites)
 //past reads
-router.get('/users/:id/books/pastReads',authenticateToken, userBooksController.getPastReads)
+router.get('/users/:id/books/pastReads', authenticateToken, userBooksController.getPastReads)
 
 // TODO: starting from get friends' current reads books in app.js
 
