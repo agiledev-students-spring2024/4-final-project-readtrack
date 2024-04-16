5a9987da8db16f94669af8ba93047755504f280f
jeffreysaeteros <js11692@nyu.edu>
2024-04-16 11:44:20 -0400

    req -> dotenv

--- back-end/src/api/middleware/authMiddleware.js
+++ back-end/src/api/middleware/authMiddleware.js
@@ -1,22 +1,25 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 exports.authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
+    console.log('authHeader: ', authHeader)
     if (!authHeader) {
         return res.status(401).json({ message: "Authorization header is missing." });
     }
 
     const token = authHeader.split(' ')[1];
+    console.log('token in authenticateToken: ', token)
     if (!token) {
         return res.status(401).json({ message: "Bearer token not found." });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
         if (err) {
-            const message = 
+            const message =
                 err.name === "JsonWebTokenError" ? "Unauthorized: Invalid token" :
-                err.name === "TokenExpiredError" ? "Unauthorized: Token has expired" :
-                "Unauthorized: Token verification failed";
+                    err.name === "TokenExpiredError" ? "Unauthorized: Token has expired" :
+                        "Unauthorized: Token verification failed";
             return res.status(403).json({ message });
         }
 
