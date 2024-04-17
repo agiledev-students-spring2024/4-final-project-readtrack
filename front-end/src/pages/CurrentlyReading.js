1bd0040e0cdcaa6b347581504476261e65179679
ellispinsky <ellispinsky5@gmail.com>
2024-04-17 14:32:30 -0400

    Refactor CurrentlyReading component to update user's currently reading list

--- front-end/src/pages/CurrentlyReading.js
+++ front-end/src/pages/CurrentlyReading.js
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
 
-function CurrentlyReading() {
+function CurrentlyReading({ userId, bookId }) {
   const [bookAdded, setBookAdded] = useState(false);
-  
+
   function UpdateCurrentlyReading() {
-    // add/ remove this book to the user's "Finished Reading Book List" update button display below
+    const token = localStorage.getItem("token");
+    const url = `http://localhost:3001/api/users/${userId}/currentlyReading`;
+    const method = bookAdded ? "DELETE" : "POST";
     setBookAdded(!bookAdded);
+    fetch(url, {
+      method: method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ bookId }),
+    })
+      .then((response) => {
+        if (!response.ok)
+          throw new Error("Failed to update currently reading list");
+        return response.json();
+      })
+      .then(() => {
+        setBookAdded(!bookAdded);
+      })
+      .catch((error) => {
+        console.error("Error updating currently reading list:", error);
+      });
   }
 
   return (
     <button className="btn-sm" onClick={UpdateCurrentlyReading}>
       {/* Add to Finished Books */}
-      {bookAdded ? 'Remove from Currently Reading' : 'Add to Currently Reading'}
+      {bookAdded ? "Remove from Currently Reading" : "Add to Currently Reading"}
     </button>
   );
-};
+}
 
 export default CurrentlyReading;
