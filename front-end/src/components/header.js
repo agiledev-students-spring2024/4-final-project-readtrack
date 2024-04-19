015c8bb89efbf99c815c68e8614a0970629e72ad
cnnrdel <cmd9609@nyu.edu>
2024-04-19 14:39:24 -0400

    stash

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Header = ({ title }) => {
   return (
-    <div className="flex flex-col items-start px-6 pt-8 pb-2">
+    <div className="flex flex-col items-start px-6 pt-8">
       <h1 className="text-4xl pb-2 text-goodreads-black font-cormorantGaramondSemibold">
         {title}
       </h1>
