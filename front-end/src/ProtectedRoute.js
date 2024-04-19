a0339b0292a93d60d6da89ea3ccc300f2f9725dd
jeffreysaeteros <js11692@nyu.edu>
2024-04-19 17:10:20 -0400

    refac: config file for preserving user state on refresh

--- front-end/src/ProtectedRoute.js
+++ front-end/src/ProtectedRoute.js
@@ -1,12 +1,23 @@
-// ProtectedRoute.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const ProtectedRoute = ({ children, loggedInUser }) => {
   const location = useLocation();
   const token = localStorage.getItem('token');
+  const storedUser = localStorage.getItem('loggedInUser');
 
-  if (!loggedInUser || !token) {
+  console.log('loggedInUser in ProtectedRoute: ', loggedInUser);
+  console.log('token in ProtectedRoute: ', token);
+
+  useEffect(() => {
+    if (storedUser && token) {
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+  }, [storedUser, token]);
+
+  if (!storedUser || !token) {
+    console.log('navigating to login')
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
